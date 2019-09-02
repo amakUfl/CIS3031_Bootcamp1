897fd2684e4eb3842e4e53bea345928f0f390c3b
amakUfl <a.makarevitch@ufl.edu>
2019-09-02 18:54:17 -0400

    Implementation of server, serving json

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ var listingData, server;
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
+  if (parsedUrl.path == "/listings" | "/listings/") {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.write(listingData);
+    response.end();
+  } else {
+    response.writeHead(404);
+    response.write('Bad gateway error');
+    response.end();
+  }
+
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
@@ -38,14 +48,16 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
    */
 
     //Check for errors
-  
+    if (err) console.log(err);
 
    //Save the sate in the listingData variable already defined
-  
+   listingData = data;
 
   //Creates the server
-  
+  server = http.createServer(requestHandler);
   //Start the server
-
-
+  server.listen(port, function() {
+    //once the server is listening, this callback function is executed
+    console.log('Server listening on: http://127.0.0.1:' + port);
+  });
 });
